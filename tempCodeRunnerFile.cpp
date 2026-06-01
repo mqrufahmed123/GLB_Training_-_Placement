dc41540636eaff6611bea3ac9c7e52d752d4ce07
Maruf Ahmed <mqrufahmed123@gmail.com>
2026-06-01 22:59:37 +0530

    written

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,5 +1,2 @@
-    vector<int> newCol = {11, 12, 13, 14};
-    int colIndex =1;    
-    for(int i = 0; i < mat.size(); i++){
-        mat[i].insert(mat[i].begin()+colIndex, newCol[i]);
-    }
+
+    // }
