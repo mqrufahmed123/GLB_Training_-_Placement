11b922628c1effae1f7bb19df29148f04475442a
Maruf Ahmed <mqrufahmed123@gmail.com>
2026-06-06 05:54:05 +0530

    added

---
+++ BootCamp1_day_wise/08_right_char_triangle.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 5;
+    for(int i = 0; i < n; i++){
+        char ch = 'A';
+        for(int j = 0; j <= i; j++){
+            cout<<ch++;
+        }
+        cout<<endl;
+    }
+}
