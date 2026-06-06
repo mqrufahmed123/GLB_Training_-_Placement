f3531a4301b25592374cd04fb816e63ad6960bc5
Maruf Ahmed <mqrufahmed123@gmail.com>
2026-06-06 05:48:55 +0530

    added

---
+++ BootCamp1_day_wise/07_number_right_pyramid.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 5;
+    for(int i = 0; i < n; i++){
+        for(int j = 1; j <= i+1; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }  
+}
