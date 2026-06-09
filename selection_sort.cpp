31d4ec5b8523f480c491c3e2b045dd5e380d24dd
Maruf Ahmed <mqrufahmed123@gmail.com>
2026-06-10 00:12:59 +0530

    added

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -4,7 +4,6 @@ int main(){
     int arr[] = {5, 4, 3, 2, 1};
     int n = size(arr);
     
-  
         for(int i = 0; i < n ; i++){
             int minIndx = i;
             for(int j = i; j < n; j++){
