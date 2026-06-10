7f20449cbe39d8ec76977bd328b9811f2252dde0
Maruf Ahmed <78859855+mqrufahmed123@users.noreply.github.com>
2026-06-10 22:08:43 +0530

    Time: 65 ms (52.84%) | Memory: 60.4 MB (77.95%) - LeetSync

--- 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
+++ 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string kthLargestNumber(vector<string>& nums, int k) {
-        sort(nums.begin(), nums.end(), [](string a, string b){
+        sort(nums.begin(), nums.end(), [](string &a, string &b){
                 if(a.size() != b.size()){
                     return a.size() > b.size();
                 }
