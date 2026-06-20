fbf78d4768bf41b36cc16bfbd0403d1ce142b2a4
Maruf Ahmed <mqrufahmed123@gmail.com>
2026-06-21 03:22:34 +0530

    added

--- trapping_rainwater.cpp
+++ trapping_rainwater.cpp
@@ -3,7 +3,27 @@
 using namespace std;
 
 int optimal(vector<int>& arr){
-    int n = arr.size();
+    int left = 0; 
+    int right = arr.size() -1;
+
+    while(left <= right){
+        if(arr[left] <= arr[right]){
+            if(arr[left]>=leftMax){
+                leftMax = arr[left];
+            }else{
+                water += leftMax - arr[left];
+            }
+            left++;
+        }else{
+            if(arr[right] >= rightMax){
+                rightMax = arr[right];
+            }else{
+                water += rightMax - arr[right];
+            }
+            right--;
+        }
+    }
+    return water;
 }
 
 int maxwaterbetter(vector<int>& arr){
